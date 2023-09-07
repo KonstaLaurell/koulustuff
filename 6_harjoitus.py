c32460e5628a0f5639a69c5050b4467a45e3bff0
Konsta Laurell <142486971+KonstaLaurell@users.noreply.github.com>
2023-09-07 10:20:56 +0300

    Update 6_harjoitus.py

--- 6_harjoitus.py
+++ 6_harjoitus.py
@@ -1,1 +1,8 @@
-
+from collections import Counter
+Nimilista: ["Petra", "Heidi", "Janne", "Petra", "Heidi", "Petra", 
+"Petra"]
+Lista: [1, 1, 2, 3, 1, 5, 2, 4, 3]
+unique = list(set(Nimilista))
+print(unique)
+unique = list(set(Lista))
+print(unique)
