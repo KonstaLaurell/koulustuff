d720256ce29dbdfb430a84cbf83bdb511f8cda75
Konsta Laurell <142486971+KonstaLaurell@users.noreply.github.com>
2023-09-07 12:32:40 +0300

    Update 5_harjoitus.py

--- 5_harjoitus.py
+++ 5_harjoitus.py
@@ -1,1 +1,19 @@
+bitit = input("Anna bitit: ")
+pyöristy = input("Anna pyöritysten lukumäärä: ")
+suunta = input("Anna pyörityksen suunta (v/o): ")
 
+try:
+    pyöristy = int(pyöristy)
+except:
+    print("sen piti olla numero")
+    
+if suunta == "v":
+    for i in range(pyöristy):
+        bitit = bitit[1:]+bitit[0]
+    print(bitit)
+elif suunta == "o":
+        for i in range(pyöristy):
+            bitit = bitit[len(str(bitit))-1]+bitit[0:len(bitit)-1]
+        print(bitit)
+else:
+    print("laita o tai v")
