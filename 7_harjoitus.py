8073a40e30ca3b51e95011be827e8c68f5230f8a
Konsta Laurell <konsta.laurell@edu.salpaus.fi>
2023-09-13 18:33:54 +0300

    7 tehtävä

--- 7_harjoitus.py
+++ 7_harjoitus.py
@@ -1,16 +1,55 @@
 import random
-lotto_rivi = input("anna 7 numeroa 1-40 : ")
-lotto_rivi.split()
+def input_lotto_numbers():
+    while True:
+        lotto_rivi = input("Anna 7 numeroa 1-40 erotettuna välilyönnillä: ").split()
+        if len(lotto_rivi) != 7:
+            print("Syötä tarkalleen 7 numeroa.")
+            continue
+        try:
+            lotto_rivi = [int(num) for num in lotto_rivi]
+            if all(1 <= num <= 40 for num in lotto_rivi):
+                return lotto_rivi
+            else:
+                print("Kaikkien numeroiden tulee olla välillä 1-40.")
+        except ValueError:
+            print("Virheellinen syöte. Kaikkien numeroiden tulee olla kokonaislukuja.")
+lotto_rivi = input_lotto_numbers()
+montako = input("monta kertaa numerot arvotaan: ")
 print(lotto_rivi)
-if isinstance(lotto_rivi, list):
-    print("on lista")
+montako = int(montako)
+nelja_oikein = 0
+viisi_oikein = 0
+kuusi_oikein = 0
+seitseman_oikein = 0
+monta_kertaa = 0
+
+
 if len(lotto_rivi) == 7:
-    for i in lotto_rivi:
-        try: 
-            i = int(i)
-        except: 
-            print("error ei toimi.exe")
-            print(ValueError)
-        
-else:
-    print("not 7 lenght")
+    for i in range(montako):
+        voittavatnumerot = []
+
+        while len(voittavatnumerot) < 7: 
+
+            uusinumero = random.randint(1,40)
+            if uusinumero not in voittavatnumerot:
+                voittavatnumerot.append(uusinumero)
+        oikein_menneet = set(voittavatnumerot).intersection(set(lotto_rivi))
+        oikein_lkm = len(oikein_menneet)
+        monta_kertaa += 1
+        if oikein_lkm == 4:
+             nelja_oikein += 1
+        elif oikein_lkm == 5:
+            viisi_oikein += 1
+        elif oikein_lkm == 6:
+            kuusi_oikein += 1
+        elif oikein_lkm == 7:
+            seitseman_oikein += 1
+    else:
+        print("not 7 lenght")
+
+print(f"{nelja_oikein} kertaa neljä oikein!")
+print(f"{viisi_oikein} kertaa viisi oikein!")
+print(f"{kuusi_oikein} kertaa kuusi oikein!")
+print(f"{seitseman_oikein} kertaa seitsemän oikein!")     
+print(f"arvottiin {monta_kertaa} kertaa")   
+    
