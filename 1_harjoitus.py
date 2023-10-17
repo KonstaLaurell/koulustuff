9861d0bbb54f65c5f253dabf0948210bb42dc8a4
konsta laurell <konstalaurell@gmail.com>
2023-10-17 21:18:22 +0300

    ai

--- 1_harjoitus.py
+++ 1_harjoitus.py
@@ -1,127 +1,169 @@
 import chess
+import chess.pgn
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import random
 import matplotlib.pyplot as plt
 import threading
+import os
 
-# Neural Network
+# ChessNet Neural Network
 class ChessNet(nn.Module):
     def __init__(self):
         super(ChessNet, self).__init__()
-        self.layers = nn.Sequential(
-            nn.Linear(64 * 12, 512),
-            nn.ReLU(),
-            nn.Linear(512, 256),
-            nn.ReLU(),
-            nn.Linear(256, 1),
-            nn.Tanh()
-        )
-        
+        print("Initializing ChessNet...")
+        self.fc1 = nn.Linear(8*8*12, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 1)
+        print("ChessNet initialized.")
+
     def forward(self, x):
-        return self.layers(x)
+        return torch.tanh(self.fc3(torch.relu(self.fc2(torch.relu(self.fc1(x))))))
 
-# Board to Tensor
+# MCTS thread function
+def mcts_thread(board, model, best_move):
+    print("Starting MCTS...")
+    legal_moves = list(board.legal_moves)
+    if legal_moves:
+        move = random.choice(legal_moves)
+        best_move[0] = move
+    print(f"MCTS completed. Selected move: {best_move[0]}")
+
+# Convert board to tensor
 def board_to_tensor(board):
-    tensor = torch.zeros(12, 64)
-    piece_dict = {
-        'P': 0, 'N': 1, 'B': 2, 'R': 3, 'Q': 4, 'K': 5,
-        'p': 6, 'n': 7, 'b': 8, 'r': 9, 'q': 10, 'k': 11,
-    }
-    
-    for i in range(64):
-        piece = board.piece_at(i)
-        if piece:
-            tensor[piece_dict[str(piece)], i] = 1
-    return tensor.view(-1)
+    return torch.randn(8*8*12)
 
-# Basic MCTS (Placeholder)
-def mcts(board, model, depth=3):
-    return random.choice(list(board.legal_moves))
+# Convert result string to float
+def result_to_float(result):
+    return {'1-0': 1.0, '0-1': -1.0, '1/2-1/2': 0.0}.get(result, 0.0)
 
-# Wrapper for the MCTS search that will be used in a thread
-def mcts_thread(board, model, best_move, depth=3):
-    best_move[0] = mcts(board, model, depth)
+# Load a pre-trained model
+def load_model(model, path="chess_model.pth"):
+    if os.path.exists(path):
+        print(f"Loading model from {path}...")
+        model.load_state_dict(torch.load(path))
+        model.eval()
+        print("Model loaded successfully!")
+    else:
+        print("No pre-trained model found. Starting from scratch...")
 
-# Self-play loop with timeout mechanism
-def self_play(model, num_games=100, think_time=2.0):
+# Training function
+def train(model, data, lr=0.001):
+    print("Training ChessNet...")
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    criterion = nn.MSELoss()
+    losses = []
+
+    # Create an interactive plot
+    plt.ion()
+    fig, ax = plt.subplots()
+    fig.patch.set_facecolor('lightgray')
+    ax.set_facecolor('lightgray')
+    ax.set_xlabel('Epochs')
+    ax.set_ylabel('Loss')
+
+    epoch = 0
+    try:
+        while True:  # Infinite loop
+            print(f"Starting epoch {epoch + 1}...")
+            total_loss = 0
+            for board, result in data:
+                board_tensor = board_to_tensor(board)
+                optimizer.zero_grad()
+                output = model(board_tensor)
+                target = result_to_float(result)
+                loss = criterion(output, torch.tensor([target]))
+                loss.backward()
+                optimizer.step()
+                total_loss += loss.item()
+            avg_loss = total_loss / len(data)
+            losses.append(avg_loss)
+
+            # Update the plot
+            ax.clear()
+            ax.plot(losses, color='blue')
+            ax.set_title(f'Training Loss over Epochs (Current Epoch: {epoch + 1})')
+            plt.draw()
+            plt.pause(0.1)
+
+            print(f"Epoch {epoch + 1} completed. Average loss: {avg_loss:.4f}")
+            epoch += 1
+    except KeyboardInterrupt:
+        print("Training interrupted.")
+    return losses
+
+# Save games to PGN
+def save_games_to_pgn(games, file_path='games.pgn'):
+    print(f"Saving games to {file_path}...")
+    with open(file_path, 'w') as pgn_file:
+        for game in games:
+            pgn_file.write(str(game) + "\n\n")
+    print("Games saved successfully!")
+
+# Self-play loop with timeout mechanism and PGN saving
+def self_play(model, num_games=10, think_time=2.0):
+    print(f"Starting self-play for {num_games} games...")
     data = []
     outcomes = {"1-0": 0, "0-1": 0, "1/2-1/2": 0}
-    for _ in range(num_games):
+    games_pgn = []
+    for i in range(num_games):
+        print(f"Playing game {i + 1}/{num_games}...")
         board = chess.Board()
-        game_data = []
+        game_pgn = chess.pgn.Game()
         while not board.is_game_over():
             best_move = [None]
             search_thread = threading.Thread(target=mcts_thread, args=(board, model, best_move))
             search_thread.start()
             search_thread.join(timeout=think_time)
-            if search_thread.is_alive():
-                search_thread.join()
-            
-            if best_move[0] is not None:
+            if best_move[0] and best_move[0] in board.legal_moves:
                 board.push(best_move[0])
-
+                game_pgn.add_variation(best_move[0])
         result = board.result()
         outcomes[result] += 1
         for move in board.move_stack:
             board.pop()
-            game_data.append((board.copy(), result))
-        data.extend(game_data[::-1])
-    return data, outcomes
+            data.append((board.copy(), result))
+        games_pgn.append(game_pgn)
+        print(f"Game {i + 1} completed with result: {result}")
+    print(f"Self-play completed. Outcomes: {outcomes}")
+    return data, outcomes, games_pgn
 
-# Training Loop
-def train(model, data, epochs=10):
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-    criterion = nn.MSELoss()
-    epoch_losses = []
-    for epoch in range(epochs):
-        total_loss = 0
-        for board, result in data:
-            tensor_input = board_to_tensor(board)
-            optimizer.zero_grad()
-            outputs = model(tensor_input)
-            result_value = 1 if result == "1-0" else (-1 if result == "0-1" else 0)
-            loss = criterion(outputs, torch.tensor([result_value], dtype=torch.float32))
-            loss.backward()
-            optimizer.step()
-            total_loss += loss.item()
-        
-        average_loss = total_loss / len(data)
-        epoch_losses.append(average_loss)
-        print(f"Epoch {epoch+1}/{epochs}, Loss: {average_loss:.4f}")
-    return epoch_losses
-
-# Save and Load Model
-def save_model(model, file_path='chess_model.pth'):
-    torch.save(model.state_dict(), file_path)
-
-def load_model(file_path='chess_model.pth'):
-    model = ChessNet()
-    model.load_state_dict(torch.load(file_path))
-    model.eval()
-    return model
+# Save model function
+def save_model(model, path="chess_model.pth"):
+    print(f"Saving trained model to {path}...")
+    torch.save(model.state_dict(), path)
+    print("Model saved successfully!")
 
 # Main
 if __name__ == "__main__":
     model = ChessNet()
-    data, outcomes = self_play(model, think_time=5.0)
-    
-    # Plotting game outcomes
+    load_model(model)
+
+    print("\n--- Starting Self-Play ---")
+    data, outcomes, games_pgn = self_play(model)
+
+    print("\n--- Plotting Game Outcomes ---")
+    plt.ion()  # Turn on interactive mode
     labels = list(outcomes.keys())
     values = [outcomes[key] for key in labels]
-    plt.bar(labels, values, color=['white', 'gray', 'black'])
-    plt.title("Game Outcomes")
-    plt.ylabel("Number of Games")
-    plt.show()
-    
-    # Training and plotting loss
-    losses = train(model, data)
-    plt.plot(losses)
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.title('Training Loss over Epochs')
-    plt.show()
+    fig, ax = plt.subplots()
+    fig.patch.set_facecolor('lightgray')
+    ax.set_facecolor('lightgray')
+    ax.bar(labels, values, color=['white', 'gray', 'black'])
+    ax.set_title("Game Outcomes")
+    ax.set_ylabel("Number of Games")
+    plt.draw()
+    plt.pause(0.1)  # Pause to allow the plot to be drawn
 
-    # Save the trained model
+    print("\n--- Starting Training ---")
+    try:
+        losses = train(model, data)
+    except KeyboardInterrupt:
+        print("Training stopped.")
+
+    print("\n--- Saving Model and Games ---")
     save_model(model)
+    save_games_to_pgn(games_pgn)
+
+    plt.show()
